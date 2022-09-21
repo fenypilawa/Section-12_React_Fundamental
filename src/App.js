04266a0669bf6a616f68474ff43137bf0b78b608
feny pilawa <pilawafn10@gmail.com>
2022-09-21 09:16:21 +0700

    Edit code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      title: "To Do List",
+      todo: [
+        <div className='Isi'>
+            <p className='satu'><span style = {{textDecoration: "line-through"}}> Membuat Komponen </span></p>
+            <p className='dua'> Unit Testing </p>
+            <p className='tiga'><span style = {{textDecoration: "line-through"}}> Setup Development Environment </span></p>
+            <p className='empat'> Membuat Komponen </p>
+        </div>
+      ],
+      todoInput: "",
+    };
+  }
+
+  handleTodoInput(event){
+    this.setState({
+        todoInput: event.target.value,
+    });
+  }
+
+  handleNew(){
+    const newTodo = this.state.todo.concat(this.state.todoInput);
+    this.setState({
+        todo: newTodo,
+        todoInput: "",
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h2>{this.state.title}</h2>
+        <hr/>
+        <input type="text" value={this.state.todoInput}
+        onChange={(event) => this.handleTodoInput(event)}/>
+        <button onClick={() => this.handleNew()}> New </button>
+        {
+          this.state.todo.map (todo => {
+            return <p>{todo}</p>
+          })
+        }
     </div>
-  );
+    );
+  }
+
 }
 
 export default App;
